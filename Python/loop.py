3ba48a9baf371edd720a05d0af042c19ffd97a6d
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2019-03-11 18:04:29 +0900

    Python 出力とifとloop

---
+++ Python/loop.py
@@ -0,0 +1,20 @@
+# 繰り返し文
+
+# while 条件式:
+#     処理
+testWhile = 1
+while testWhile < 4:
+    print(testWhile)
+    testWhile += 1
+# 1
+# 2
+# 3
+
+# for 変数名 in リスト(辞書):
+#     処理
+list = ['A', 'B', 'C']
+for value in list:
+    print(value)
+# A
+# B
+# C
