742a272ca3275b00878f4be11602bbd46482093c
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2019-04-30 13:18:51 +0900

    改行が抜けていたので修正

--- PHP/Loop.php
+++ PHP/Loop.php
@@ -3,6 +3,7 @@
 // 制御
 // ループから抜ける：break
 // 次のループへ：continue
+
 /* while ( 条件式 ) {
  *     処理;
  * }
@@ -14,6 +15,7 @@ while ($testWhile < 4) {
 }
 // 123
 echo "\n";
+
 /* do {
  *    処理;
  * } while (条件式);
@@ -26,6 +28,7 @@ do {
 } while ($testDo < 4);
 // 123
 echo "\n";
+
 /* for (変数定義; 条件式; 更新処理) {
  *     処理;
  * }
@@ -35,6 +38,7 @@ for ($i = 0; $i < 3; $i++) {
 }
 // 012
 echo "\n";
+
 /* foreach (配列 as 変数名) {
  *     処理;
  * }
@@ -45,6 +49,7 @@ foreach ($list as $value) {
 }
 // ABCDE
 echo "\n";
+
 /* foreach (配列 as キー => 値) {
  *     処理;
  * }
