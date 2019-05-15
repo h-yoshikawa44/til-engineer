ab6b87e3e50bec2b0f9570cbb431844fdb1752fc
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2019-05-15 17:21:12 +0900

    Laravel コミットもれ

--- PHP/Laravel_tutorial/src/routes/web.php
+++ PHP/Laravel_tutorial/src/routes/web.php
@@ -82,3 +82,11 @@ Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
 Route::get('person/add', 'PersonController@add');
 Route::post('person/add', 'PersonController@create');
+Route::get('person/edit', 'PersonController@edit');
+Route::post('person/edit', 'PersonController@update');
+Route::get('person/del', 'PersonController@delete');
+Route::post('person/del', 'PersonController@remove');
+
+Route::get('board', 'BoardController@index');
+Route::get('board/add', 'BoardController@add');
+Route::post('board/add', 'BoardController@create');
