1f12a48407e76dd7ec5286e28eb302277a885084
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2019-07-22 14:37:43 +0900

    Railチュートリアル 9章

--- Ruby/Rails_tutorial/src/db/schema.rb
+++ Ruby/Rails_tutorial/src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_054356) do
+ActiveRecord::Schema.define(version: 2019_07_22_035537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_054356) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
