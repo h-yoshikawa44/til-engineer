565b1830baefbdebd8512987250d1358d74a0e63
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2019-07-25 13:01:30 +0900

    Railsチュートリアル 10章 ～10.1.3

--- Ruby/Rails_tutorial/src/app/controllers/users_controller.rb
+++ Ruby/Rails_tutorial/src/app/controllers/users_controller.rb
@@ -18,6 +18,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
+
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
