50770665cddeca2546eebbb9575572649e47b736
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2019-08-01 15:40:01 +0900

    Railsチュートリアル 12章

--- Ruby/Rails/src/config/routes.rb
+++ Ruby/Rails/src/config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- Ruby/Rails/src/db/schema.rb
+++ Ruby/Rails/src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_045353) do
+ActiveRecord::Schema.define(version: 2019_08_01_055231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_07_30_045353) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
