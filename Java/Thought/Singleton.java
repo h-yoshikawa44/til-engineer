7ede515f9ee81ffee5e067737e6ca213ebfcf9b5
h-yoshikawa0724 <guardian.yatagaras@icloud.com>
2020-04-12 17:35:33 +0900

    細かな修正、不要なスタイルの削除

--- Java/Thought/Singleton.java
+++ Java/Thought/Singleton.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Singleton {
     public static void main(String[] args) throws Exception {
         MySingleton i1 = MySingleton.getInstance();
         MySingleton i2 = MySingleton.getInstance();
