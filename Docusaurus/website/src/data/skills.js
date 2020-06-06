1935146067126b454079fcdecd7ff96905352daf
h-yoshikawa0724 <guardian.yatagaras@icloud.com>
2020-06-06 18:50:42 +0900

    ポートフォリオのデータ部分を別ファイルに分離

---
+++ Docusaurus/website/src/data/skills.js
@@ -0,0 +1,190 @@
+import React from 'react';
+
+export const editors = [
+  {
+    title: <>Visual Studio Code</>,
+    imageUrl: 'img/logo-icons/visual-studio-code.svg',
+    description: (
+      <>
+        Java以外の言語のコーディングで使用。
+      </>
+    ),
+  },
+  {
+    title: <>Eclipse</>,
+    imageUrl: 'img/logo-icons/eclipse.svg',
+    description: (
+      <>
+        Javaのコーディングで使用。
+      </>
+    ),
+  },
+   {
+    title: <>Unity</>,
+    imageUrl: 'img/logo-icons/unity.svg',
+    description: (
+      <>
+        研修で使用。正直あまり覚えていない。
+      </>
+    ),
+  },
+];
+
+export const environments = [
+  {
+    title: <>Git</>,
+    imageUrl: 'img/logo-icons/git-icon.svg',
+    description: (
+      <>
+        普段の案件業務から、個人プロジェクトにわたり使用。差分はVSCodeなどGUIで確認し、コマンドはCLIで操作することが多い。
+      </>
+    ),
+  },
+  {
+    title: <>Docker</>,
+    imageUrl: 'img/logo-icons/docker-icon.svg',
+    description: (
+      <>
+        実務使用経験あり。また、Dockerを使用した環境構築手順のQiita記事を書いた経験あり。
+      </>
+    ),
+  }
+];
+
+export const langAndframewarks = [
+  {
+    title: <>HTML</>,
+    imageUrl: 'img/logo-icons/html-5.svg',
+    description: (
+      <>
+        基本的には研修で学んだ程度。そのほか、FWのビューで記述したり、ReactのJSX（厳密には異なるが）で書いているなど。
+      </>
+    ),
+  },
+  {
+    title: <>CSS</>,
+    imageUrl: 'img/logo-icons/css-3.svg',
+    description: (
+      <>
+        実務使用経験あり。
+      </>
+    ),
+  },
+  {
+    title: <>JavaScript</>,
+    imageUrl: 'img/logo-icons/javascript.svg',
+    description: (
+      <>
+        実務使用経験ありであるが、基本的にフロントのみ。あとはGoogle Apps Scriptの経験あり。
+      </>
+    ),
+  },
+  {
+    title: <>React</>,
+    imageUrl: 'img/logo-icons/react.svg',
+    description: (
+      <>
+        実務使用経験あり。自分のスキルのうち、フロントでメインにしたい技術。
+      </>
+    ),
+  },
+  {
+    title: <>Docusaurus</>,
+    imageUrl: 'img/logo-icons/docusaurus.svg',
+    description: (
+      <>
+        当サイトで使用。v1は少々使いづらさを感じたので、v2を使用。
+      </>
+    ),
+  },
+  {
+    title: <>Java</>,
+    imageUrl: 'img/logo-icons/java.svg',
+    description: (
+      <>
+        実務使用経験あり。この業界に入る前にメインで学んでいた言語で、自分のスキルのうち、バックエンドでメインにしたい言語。
+      </>
+    ),
+  },
+  {
+    title: <>Spring</>,
+    imageUrl: 'img/logo-icons/spring.svg',
+    description: (
+      <>
+        実務使用経験あり。テンプレートエンジンは使用せず、APIとしての開発経験のみ。
+      </>
+    ),
+  },
+  {
+    title: <>PHP</>,
+    imageUrl: 'img/logo-icons/php.svg',
+    description: (
+      <>
+        実務使用経験あり。個人勉強もしている言語。
+      </>
+    ),
+  },
+  {
+    title: <>CakePHP</>,
+    imageUrl: 'img/logo-icons/cakephp.svg',
+    description: (
+      <>
+        実務使用経験あり。最初の案件で使用したFWであるため、ブランクあり。
+      </>
+    ),
+  },
+  {
+    title: <>Laravel</>,
+    imageUrl: 'img/logo-icons/laravel.svg',
+    description: (
+      <>
+        個人勉強しているのみのFW。
+      </>
+    ),
+  },
+  {
+    title: <>Python</>,
+    imageUrl: 'img/logo-icons/python.svg',
+    description: (
+      <>
+        実務使用経験あり。AWS LambdaをPythonで書いていた経験のみ。
+      </>
+    ),
+  },
+  {
+    title: <>Ruby</>,
+    imageUrl: 'img/logo-icons/ruby.svg',
+    description: (
+      <>
+        研修で学んだ程度。
+      </>
+    ),
+  },
+  {
+    title: <>Rails</>,
+    imageUrl: 'img/logo-icons/rails.svg',
+    description: (
+      <>
+        研修で学んだあと、Railsチュートリアルを学んだ程度。
+      </>
+    ),
+  },
+  {
+    title: <>Jekyll</>,
+    imageUrl: 'img/logo-icons/jekyll.svg',
+    description: (
+      <>
+        自ブログで使用。テーマは公開されているものを使用しているが、それをカスタマイズして使用。
+      </>
+    ),
+  },
+  {
+    title: <>C#</>,
+    imageUrl: 'img/logo-icons/c-sharp.svg',
+    description: (
+      <>
+        研修で学んだ程度。
+      </>
+    ),
+  },
+];
