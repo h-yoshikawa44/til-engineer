5052dbcbed62112dc526479354800e47eb15a3d6
h-yoshikawa0724 <guardian.yatagaras@icloud.com>
2020-06-13 14:57:28 +0900

    myProductのデータを一部修正

--- Docusaurus/website/src/data/products.js
+++ Docusaurus/website/src/data/products.js
@@ -1,7 +1,13 @@
+import React from 'react';
+
 const eventProducts = [
   {
     productName: 'Jigsaw Like Puzzle',
-    description: 'web1week（1週間でwebサービスを作るイベント）のお題「Like」の投稿作品。',
+    description: (
+      <>
+      web1week（1週間でwebサービスを作るイベント）のお題「Like」の投稿作品。ジグソーパズルっぽいパズルゲーム。<br />※PC向け。
+      </>
+    ),
     imageUrl: 'img/my-products/jigsaw-like-puzzle/overview.png',
     url: 'https://jigsaw-like-puzzle.web.app',
     githubUrl: 'https://github.com/h-yoshikawa0724/jigsaw-like-puzzle'
