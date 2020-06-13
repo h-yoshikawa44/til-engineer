8f7c649979c6b72446325450d5395a8627b66721
h-yoshikawa0724 <guardian.yatagaras@icloud.com>
2020-06-13 14:10:42 +0900

    勉強用Rails環境のgemバージョンアップ対応

--- Ruby/Rails/src/config/initializers/assets.rb
+++ Ruby/Rails/src/config/initializers/assets.rb
@@ -12,3 +12,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# エラー対策（キャッシュを作成する場所を変える） - File exists @ dir_s_mkdir - /src/tmp/cache/assets/sprockets/v3.0/a1
+# https://crieit.net/posts/Windows-Docker-Rails-sprockets
+Rails.application.config.assets.configure do |env|
+  env.cache = Sprockets::Cache::FileStore.new(
+    ENV.fetch("SPROCKETS_CACHE", "#{env.root}/tmp/cache/assets"),
+    Rails.application.config.assets.cache_limit,
+    env.logger
+  )
+end
